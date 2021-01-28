35c5ab42d2df42210444ad4c5d11a0eeb23e15fa
MariskaS85 <mcfsnijdewind@gmail.com>
2021-01-28 16:23:31 +0000

    added products go-up button and sort all funtion

--- products/views.py
+++ products/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_list_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
+
 # Create your views here.
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -21,7 +24,7 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-                
+
             if sortkey == 'category':
                 sortkey = 'category__name'
 
