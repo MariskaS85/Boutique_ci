1008aefa58a1b257afce7199c89d6ae866e629da
MariskaS85 <mcfsnijdewind@gmail.com>
2021-01-31 14:39:54 +0000

    added shopping bag context processor

--- boutique_ci/settings.py
+++ boutique_ci/settings.py
@@ -113,7 +113,7 @@ WSGI_APPLICATION = 'boutique_ci.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 
@@ -69,4 +70,4 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/product_detail.html', context)
